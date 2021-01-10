7a71f79e1cef6ddf51d50b7af549ab977d4d6385
edreitlein <67922907+edreitlein@users.noreply.github.com>
2021-01-10 18:34:38 -0500

    main

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 let nameInput;
+//          btns    0,   1    2    3   4    5    6    7    8
+let buttonArray = [null,null,null,null,null,null,null,null,null]
 let xoro = 0;//tracks if the button for tic tac toe is going to be an X or and O, 0 = O, 1 = X
 function grabInput(){//this gets the input from the button myText and alerts it to screen using the .value to grab the HTMLinputelement's value as String
             nameInput = document.getElementById("myText");
@@ -11,9 +13,10 @@ function displayInput(){//displays the input from grabInput() and myText button
 
 }
 
-
+//this function in obsolute for making a tictactoe game as game should never be more then 3x3
 function makeButtons(x,y){//takes 2 inputs and makes a grid of buttons x in height and y
     console.log(x,y);
+    
     let i, j;
     for (i =0;i<x;i++){
         for (j=0;j<y;j++){
@@ -41,3 +44,7 @@ function ticTac(element){//Takes the element of the button clicked/sets the butt
 }
 
 //TODO check if a winner has been made with tictactoe - ideas is store values in an 2d array and check for wins
+function setTicTacArray(buttonID){//takes a buttonID and sets it corosponding array value to X or O for tic tac toe
+
+
+}
